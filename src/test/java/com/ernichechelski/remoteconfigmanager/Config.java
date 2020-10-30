6612c7a2f7967bed958a09cc7b0bcdd3be9c3252
Ernest Chechelski <ernichechelski@gmail.com>
2020-10-30 11:46:19 +0100

    Testing with projectID

--- src/test/java/com/ernichechelski/remoteconfigmanager/Config.java
+++ src/test/java/com/ernichechelski/remoteconfigmanager/Config.java
@@ -7,7 +7,7 @@ import com.ernichechelski.remoteconfigmanager.utils.ParameterContainer;
  */
 public class Config {
 
-    public ParameterContainer<String> text = new ParameterContainer("Remote config generated from code");
+    public ParameterContainer<String> text = new ParameterContainer("Remote config projectID");
 
     public ParameterContainer<Theme> theme = new ParameterContainer(new Theme());
 
